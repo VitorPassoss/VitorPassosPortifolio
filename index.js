0f6923a6be4e8fee6682a94815c2059aadce5849
vitor passos <vitorpassosbrit@gmail.com>
2022-08-03 22:54:50 -0300

    adicionei o grid onde irei colocar os projetos

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 
 function alternar_fundo(){
+
     const body = document.body
     const Main = document.querySelector('#main')
-    const btn = document.querySelector('.button')
     const txtBTN = document.querySelector('.Link')
     const txtBTN2 = document.querySelector('.Link2')
     const n1 = document.querySelector('#Home')
@@ -20,6 +20,8 @@ function alternar_fundo(){
     const desc2 = document.querySelector('.contentTwo')
     const desc3 = document.querySelector('.contentThree')
 
+
+
     desc3.classList.toggle('textoDARK')
     desc2.classList.toggle('textoDARK')
     desc.classList.toggle('textoDARK')
@@ -30,8 +32,6 @@ function alternar_fundo(){
     title5.classList.toggle('textoDARK')
     title6.classList.toggle('textoDARK')
     title7.classList.toggle('textoDARK')
-    
-
     body.classList.toggle('fundoBRANCO')
     Main.classList.toggle('fundoBRANCO')
     n1.classList.toggle('textoDARK')
@@ -40,7 +40,12 @@ function alternar_fundo(){
     n4.classList.toggle('textoDARK')
     txtBTN.classList.toggle('textoDARK')
     txtBTN2.classList.toggle('textoDARK')
-   
- 
-   
+
 }
+
+
+url = "https://api.github.com/users"
+
+
+
+
